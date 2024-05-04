a175bd73639579b7915489653afff1e4fff73222
Matheus Gava <matheus.gava3@gmail.com>
2024-05-04 15:38:21 -0300

    Add describe route

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import base64
+import io
+import os
+from PIL import Image
 import google.generativeai as genai
-import PIL.Image
 
 app = Flask(__name__)
+
+genai.configure(api_key=os.environ.get('GOOGLE_API_KEY'))
+model = genai.GenerativeModel('gemini-pro-vision')
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/describe', methods=['POST'])
+def describe_image():
+
+    # Verifica se a requisição contém dados
+    if 'image' not in request.json:
+        return jsonify({'error': 'No image data sent in request body'}), 400
+
+    try:
+        # Decodifica a base64 para obter os bytes da imagem
+        image_data = base64.b64decode(request.json['image'])
+        
+        # Converte os bytes em um objeto de imagem
+        image = Image.open(io.BytesIO(image_data))
+
+
+        response = model.generate_content(["Descreva com detalhes", image])
+        response.resolve()
+
+        # Retorna a descrição da imagem como JSON
+        return jsonify({'description': response.text}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
